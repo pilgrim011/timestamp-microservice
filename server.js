ac4819979a2a4baf28a8a06ac726fee16c2b6205
Zoran Stanić <printerstampac@gmail.com>
2016-11-06 19:56:42 +0000

    Add Procfile to project

--- server.js
+++ server.js
@@ -1,2 +1,14 @@
-var express = require("express")
+var express = require('express');
+ 
+var app = express();
+var PORT = process.env.PORT || 8080;
+ 
+app.get('process.env.IP', function(request, response) {
+  var timestamp = request.params.timestamp;
+  response.send('Timestamp: ' + timestamp);
+});
+ 
+app.listen(PORT, function() {
+  console.log('Server is listening on port ' + PORT);
+});
 
