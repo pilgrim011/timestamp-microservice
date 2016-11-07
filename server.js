72ec526f59d3a7521eff3ad81f5a0bb844bfb791
Zoran Stanić <printerstampac@gmail.com>
2016-11-07 19:19:29 +0000

    some params defined

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 var express = require('express');
- 
 var app = express();
+var moment = require("moment");
 var PORT = process.env.PORT || 8080;
+
  
-app.get('/:time', function(request, response) {
-  var timestamp = request.params.time;
+app.get('/:query', function(request, response) {
+  var timestamp = request.params.query;
+  var natural = moment.unix(parseInt(timestamp,10)).format('MMMM D YYYY');
+  var result = { "unix": timestamp, "natural": natural };
+  if (!isNaN(timestamp)){
+      response.send(result);
+      
+  }
   
-  response.send('Timestamp: ' + timestamp);
+  
 });
  
 app.listen(PORT, function() {
